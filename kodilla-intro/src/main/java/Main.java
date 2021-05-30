c37d39ff6ff0cb4a33d93656c958bf34831550a9
Rafał Skoczek <rafal.skoczek@op.pl>
2021-05-30 12:13:12 +0200

    zmienne

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {public static void main(String[] args) {
+    String myName = "Rafał";
+    int myAge = 30;
+    double height = 1.78;
+    char carClass = 'A';
+    boolean isGoodProgrammer = true;
+
+    System.out.println(myAge + " " + height);
+
+    String zadanie = "Hello from FirstClass!";
+
+    System.out.println(zadanie);
+
+}
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String netflixSeries = "Homeland";
+        int seasonNumber = 3;
+        int episodeNumber = 18;
+        double episodeTime = 33.30;
+        char rating = 'A';
+        String comment = "dobry epizod";
+
+        System.out.println(netflixSeries + "\n" + seasonNumber + "\n" + episodeNumber + "\n" + episodeTime + "\n" + rating + " - " + comment );
+    } }
