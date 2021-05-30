61c9c0feaefc5ca3bc72b8f6e5053910f34d1c17
Rafał Skoczek <rafal.skoczek@op.pl>
2021-05-30 11:29:35 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\n" + "Rafal Skoczek\n" + "30 lat\n" + "07.10.1990");
+
+
+        System.out.println(" ");
+        System.out.println("Hello World!");
+        System.out.println("Rafal Skoczek ");
+        System.out.println("30 lat");
+        System.out.println("07.10.1990");
+    }
+
+}
