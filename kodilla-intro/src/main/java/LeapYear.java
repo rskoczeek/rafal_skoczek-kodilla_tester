f188001d5ad6c9f9c8a65af9454f595bcedb1e2a
Rafał Skoczek <rafal.skoczek@op.pl>
2021-06-01 22:29:04 +0200

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int rok = 1883 ;
+        int a = 4;
+        int b = 100;
+        int c = 400;
+        if (rok%a == 0 && rok%b > 0 || rok%c == 0)  {
+            System.out.println("rok jest rokiem przestępnym");
+        } else {System.out.println("rok nie jest rokiem przestępnym"); }
+    }
+}
--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -8,4 +8,14 @@ public class MainExtended {
         String comment = "dobry epizod";
 
         System.out.println(netflixSeries + "\n" + seasonNumber + "\n" + episodeNumber + "\n" + episodeTime + "\n" + rating + " - " + comment );
+        System.out.println("\n");
+
+        //lub każda z lini oddzielnie
+
+        System.out.println(netflixSeries);
+        System.out.println(seasonNumber);
+        System.out.println(episodeNumber);
+        System.out.println(episodeTime);
+        System.out.println(rating);
+        System.out.println(comment);
     } }
