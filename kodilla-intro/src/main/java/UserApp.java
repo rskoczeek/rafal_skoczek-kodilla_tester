492bd141d4c37cc42e0f01f740dac3dd4018accf
Rafał Skoczek <rafal.skoczek@op.pl>
2021-06-24 20:30:33 +0200

    KOD-2.5 loops

---
+++ kodilla-intro/src/main/java/UserApp.java
@@ -0,0 +1,40 @@
+public class UserApp {
+
+    public static void main(String[] args) {
+
+        User bartek = new User("Bartek", 24);
+        User zofia = new User("Zofia", 35);
+        User kasia = new User("Kasia", 19);
+        User piotr = new User("Piotr", 44);
+        User zbyszek = new User("Zbyszek", 55);
+        User[] users = {bartek, zofia, kasia, piotr, zbyszek};
+
+        System.out.println(getAvarageAge(users));
+        System.out.println(getUsersUnderAvarage(users));
+    }
+
+
+    public static double getAvarageAge(User[] users) {
+        double result = 0;
+        for (int i = 0; i < users.length; i++) {
+            result = result + users[i].getAge();
+        }
+        return result / users.length;
+    }
+    public static String getUsersUnderAvarage(User[] users) {
+        for (int i = 0; i < users.length; i++) {
+            if (getAvarageAge() > users[i].getAge(users)) {
+            return users[i].getName();
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
