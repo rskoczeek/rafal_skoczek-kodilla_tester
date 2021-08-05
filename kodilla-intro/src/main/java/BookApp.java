d19b348f1f581fafab29b8e899d7895370ce6cd4
Rafał Skoczek <rafal.skoczek@op.pl>
2021-08-05 18:42:35 +0200

    Remove duplicated classes

--- kodilla-intro/src/main/java/BookApp.java
+++
@@ -1,8 +0,0 @@
-public class BookApp {
-    public static void main(String[] args) {
-        Book book = Book.of("Sienkiewicz", "Ogniem i mieczem");
-        Book biblia = Book.of("Bibilia");
-        System.out.println(biblia);
-        System.out.println(book);
-    }
-}
