010ffdbae683e652fd6b706e57999bea3faa5f01
rskoczeek <84739292+rskoczeek@users.noreply.github.com>
2021-08-09 20:56:21 +0200

    EX.KOD.2.8 (#8)

---
+++ kodilla-intro/src/main/java/com/kodilla/intro/oop/AdvCalculator.java
@@ -0,0 +1,26 @@
+package com.kodilla.intro.oop;
+
+public class AdvCalculator {
+    public double calculate() {
+        String userSelected = UserDialogs.getUserSelection();
+        double a = UserDialogs.getValue();
+        double b = UserDialogs.getValue();
+        double result = 0;
+        switch (userSelected) {
+            case "ADD":
+                result = a + b;
+                break;                     // [1]
+            case "SUB":
+                result = a - b;
+                break;                     // [2]
+            case "DIV":
+                result = a / b;
+                break;                     // [3]
+            case "MUL":
+                result = a * b;
+                break;                     // [4]
+        }
+        return result;
+    }
+}
+
