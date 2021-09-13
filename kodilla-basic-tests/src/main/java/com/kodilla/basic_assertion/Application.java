444d11a14251f918fcb411acef4a807e2f9176da
Rafał Skoczek <rafal.skoczek@op.pl>
2021-09-13 20:10:34 +0200

    Zadanie KOD-3.5

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -3,16 +3,34 @@ package com.kodilla.basic_assertion;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 10;
+        int a = 5;
         int b = 8;
         int sumResult = calculator.sum(a,b);
-        boolean correct = ResultChecker.assertEquals(13, sumResult);
-        if (correct) {
+        int subResult = calculator.subtract(a, b);
+        int squareResult = calculator.square(a);
+
+        boolean correctSum = ResultChecker.assertEquals(13, sumResult);
+        if (correctSum) {
             System.out.println("Metoda działa poprawnie dla liczb " + a + " i " + b);
         }
-
         else {
             System.out.println("Metoda działa niepoprawnie dla liczb " + a + " i " + b);
+        }
+
+        boolean correctSubstract = ResultChecker.assertEquals(-3, subResult);
+        if (correctSubstract) {
+            System.out.println("Metoda działa poprawnie dla liczb " + a + " i " + b);
+        }
+        else {
+            System.out.println("Metoda działa niepoprawnie dla liczb " + a + " i " + b);
+        }
+
+        boolean correctSquare = ResultChecker.assertEquals(25, squareResult);
+        if (correctSquare) {
+            System.out.println("Metoda działa poprawnie dla liczby " + a );
+        }
+        else {
+            System.out.println("Metoda działa niepoprawnie dla liczby " + a);
         }
 
     }
