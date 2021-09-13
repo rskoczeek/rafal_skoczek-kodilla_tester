d2ae9eec81b3e5570e1454ea74208348101c9cff
rskoczeek <84739292+rskoczeek@users.noreply.github.com>
2021-09-13 21:58:47 +0200

    Kod 3.5 (#15)
    * EX-3.5
    
    * Przykład 3.5 dodany
    
    * Zadanie KOD-3.5

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -3,6 +3,24 @@ package com.kodilla.basic_assertion;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a,b);
+        int subResult = calculator.subtract(a, b);
+        int squareResult = calculator.square(a);
+
+        boolean correctSum = ResultChecker.assertEquals(13, sumResult);
+        if (correctSum) {
+            System.out.println("Metoda działa poprawnie dla liczb " + a + " i " + b);
+        }
+        else {
+            System.out.println("Metoda działa niepoprawnie dla liczb " + a + " i " + b);
+        }
+
+        boolean correctSubstract = ResultChecker.assertEquals(-3, subResult);
+        if (correctSubstract) {
+            System.out.println("Metoda działa poprawnie dla liczb " + a + " i " + b);
+        }
         int a = 10;
         int b = 8;
         int sumResult = calculator.sum(a,b);
@@ -13,6 +31,15 @@ public class Application {
 
         else {
             System.out.println("Metoda działa niepoprawnie dla liczb " + a + " i " + b);
+        }
+
+
+        boolean correctSquare = ResultChecker.assertEquals(25, squareResult);
+        if (correctSquare) {
+            System.out.println("Metoda działa poprawnie dla liczby " + a );
+        }
+        else {
+            System.out.println("Metoda działa niepoprawnie dla liczby " + a);
         }
 
     }
