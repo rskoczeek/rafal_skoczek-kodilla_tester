0fc7283274fd4086484a1e13af25921b27d5ced3
Rafał Skoczek <rafal.skoczek@op.pl>
2021-10-06 17:38:50 +0200

    KOD-4.1 example

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -53,20 +53,20 @@ class Triangle implements Shape {
 
 
 public class InterfacesDemo {
-
     public static void main(String[] args) {
-
         Square square = new Square(10.0);
-        System.out.println(square.getArea());
-        System.out.println(square.getPerimeter());
+        showShapeDetails(square);
 
-        Circle circle = new Circle(7);
-        System.out.println(circle.getArea());
-        System.out.println(circle.getPerimeter());
+        Circle circle = new Circle(7.0);
+        showShapeDetails(circle);
 
-        Triangle triangle = new Triangle(5,5,5);
-        System.out.println(triangle.getArea());
-        System.out.println(triangle.getPerimeter());
+        Triangle triangle = new Triangle(6.0, 4.0, 7.211);
+       showShapeDetails(triangle);
+    }
+
+    private static void showShapeDetails(Shape shape) {
+        System.out.println(shape.getArea());
+        System.out.println(shape.getPerimeter());
     }
 }
 
